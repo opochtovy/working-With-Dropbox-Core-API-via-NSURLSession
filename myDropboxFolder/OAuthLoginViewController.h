8b8bf289362d1b7c0f5b595700bca826ad337db7
Oleg Pochtovy <opochtovy@yahoo.com>
2014-08-05 15:00:46 +0300

    OAuth Login Process

---
+++ myDropboxFolder/OAuthLoginViewController.h
@@ -0,0 +1,15 @@
+//
+//  OAuthLoginViewController.h
+//  myDropboxFolder
+//
+//  Created by Oleg Pochtovy on 31.07.14.
+//  Copyright (c) 2014 Oleg Pochtovy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OAuthLoginViewController : UIViewController
+
+@property (strong, nonatomic) NSString *userToken;
+
+@end
