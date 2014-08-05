d0d7598f43c3969d1f937ad93ee4b63ae15f782f
Oleg Pochtovy <opochtovy@yahoo.com>
2014-08-05 15:22:11 +0300

    API illustrates the work with a web-service API (Dropbox Core API) via NSURLSession

---
+++ myDropboxFolder/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  myDropboxFolder
+//
+//  Created by Oleg Pochtovy on 31.07.14.
+//  Copyright (c) 2014 Oleg Pochtovy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
