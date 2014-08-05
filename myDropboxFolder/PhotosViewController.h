5ebf141cfe7a27d600e675fac654b8b1579328e6
Oleg Pochtovy <opochtovy@yahoo.com>
2014-08-05 15:06:30 +0300

    This class shows photos from our directory

---
+++ myDropboxFolder/PhotosViewController.h
@@ -0,0 +1,15 @@
+//
+//  PhotosViewController.h
+//  myDropboxFolder
+//
+//  Created by Oleg Pochtovy on 04.08.14.
+//  Copyright (c) 2014 Oleg Pochtovy. All rights reserved.
+//
+
+// To view photos in this app you need to copy the "photos" directory to your app folder on Dropbox.
+
+#import <UIKit/UIKit.h>
+
+@interface PhotosViewController : UIViewController
+
+@end
