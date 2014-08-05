b9632b8db7c63bf2993de4f53a4475b7f44e3305
Oleg Pochtovy <opochtovy@yahoo.com>
2014-08-05 14:56:02 +0300

    Initial сommit

---
+++ myDropboxFolder/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  myDropboxFolder
+//
+//  Created by Oleg Pochtovy on 31.07.14.
+//  Copyright (c) 2014 Oleg Pochtovy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
