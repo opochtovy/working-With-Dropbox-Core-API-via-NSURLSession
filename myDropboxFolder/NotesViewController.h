c7b7645502c77b4d7c7bc02771e76a5cba3f9eef
Oleg Pochtovy <opochtovy@yahoo.com>
2014-08-05 15:03:34 +0300

    The goal of this class NoteViewController is to retrieve a list of the files inside the app’s Dropbox folder

---
+++ myDropboxFolder/NotesViewController.h
@@ -0,0 +1,15 @@
+//
+//  NotesViewController.h
+//  myDropboxFolder
+//
+//  Created by Oleg Pochtovy on 01.08.14.
+//  Copyright (c) 2014 Oleg Pochtovy. All rights reserved.
+//
+
+// the goal of this class NoteViewController is to retrieve a list of the files inside the app’s Dropbox folder
+
+#import <UIKit/UIKit.h>
+
+@interface NotesViewController : UITableViewController
+
+@end
